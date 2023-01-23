63b0f9bd26fc9dab3268e4c815abdbc5dc3011b9
Michael BARRECA <Michael-73@live.Fr>
2023-01-23 19:54:44 +0100

    mise a jour

--- app.js
+++ app.js
@@ -2,18 +2,47 @@
 //Créer 3 blocks contenant 3 languages !=
 // value enter  \n
 
+
 const htmlBox = document.querySelector("#htmlBox");
 const cssBox = document.querySelector("#cssBox");
+const css = document.querySelector("#css");
 const js = document.querySelector("#jsBox");
 const result = document.querySelector("#resultBox");
-const css = document.querySelector("#css");
+const progressBar = document.querySelector(".progress-bar");
+
+
 
 htmlBox.addEventListener("keydown", addCode);
 cssBox.addEventListener("keydown", addStyle);
 
+function togg1(){
+    togg1.addEventListener.style.hidden;
+    togg2.addEventListener.style.
+}
+
+
 function addCode() {
     result.innerHTML = htmlBox.value;
+    let nbCaractere = htmlBox.value.length;
+    count .innerText = nbCaractere+'/100%'
+
+    if (nbCaractere <= 35) {
+        progressBar.style.backgroundColor="green";
+        progressBar.style.width=nbCaractere+'%';
+
+    } else if (nbCaractere <= 75) {
+            progressBar.style.backgroundColor="orange";
+            progressBar.style.width=nbCaractere+'%';
+
+        } else {
+                progressBar.style.backgroundColor="red";
+                progressBar.style.width=nbCaractere+'%';
+            }
 }
+
+
+
+
 
 function addStyle() {
     css.innerText = cssBox.value;
@@ -26,11 +55,11 @@ function addJs() {
 function run() {
     // eval()
     new Function(js.value)();
+    
 }
 
 
  const button = document.querySelector('button');
-
 
  button.addEventListener('click', event => {
    button.innerHTML = `Nombre de clics : ${event.detail}`;
